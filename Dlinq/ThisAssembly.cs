d4c43efcfa5b51eb94c2e1c7c19cc75e85c15ad3
zhaoxf101@qq.com <zhaoxf101@qq.com>
2017-04-06 14:52:57 +0800

    1

---
+++ Dlinq/ThisAssembly.cs
@@ -0,0 +1,22 @@
+using System;
+
+internal static class ThisAssembly
+{
+	internal const string Title = "System.Data.Linq.dll";
+
+	internal const string Description = "System.Data.Linq.dll";
+
+	internal const string DefaultAlias = "System.Data.Linq.dll";
+
+	internal const string Copyright = "© Microsoft Corporation.  All rights reserved.";
+
+	internal const string Version = "4.0.0.0";
+
+	internal const string InformationalVersion = "4.6.1586.0";
+
+	internal const string DailyBuildNumberStr = "30319";
+
+	internal const string BuildRevisionStr = "0";
+
+	internal const int DailyBuildNumber = 30319;
+}
